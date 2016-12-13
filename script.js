9a435f6a1c6ed90304dd5c8b2a010ce922f0f8a5
Tobias Murphy <murphy.tobias@gmail.com>
2016-12-13 12:47:49 -0800

    restored functionality, cleaned up code

--- script.js
+++ script.js
@@ -1,60 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log("DOM loaded");
 });
+reset();
 
 ///dice roll random numbers
-function random(){
-  var output = Math.random() * 10;
-  if (output <= (10/6)){
-    output = 1;
-    return output;
-  } else if (output <= ((10/6)*2)) {
-    output = 2;
-    return output;
-  }else if (output <= ((10/6)*3)) {
-    output = 3;
-    return output;
-  } else if (output <= ((10/6)*4)) {
-    output = 4;
-    return output;
-  } else if (output <= ((10/6)*5)) {
-    output = 5;
-    return output;
-  } else if (output <= ((10/6)*6)){
-    output = 6;
-    return output;
+function random(die){
+  die = Math.ceil(Math.random() * 6);
+  return die;
+}
+
+function rollDie(id) {
+  var die = $(id);
+  if (die.hasClass("active")){
+    die.removeClass("face1 face2 face3 face4 face5 face6");
+    die.animate({marginLeft: (Math.random()*2.3)* 100}, 150);
+    var roll = random(die);
+    die.addClass("face" + roll);
+    return roll;
   }
 }
 
 /// roll button rolls dice
-$("#roll").click(rollDice);
-function rollDice(){
-  console.log("rolled");
-  var die1 = $("#die1").removeClass("oneface twoface threeface fourface fiveface sixface");
-  var die2 = $("#die2").removeClass("oneface twoface threeface fourface fiveface sixface");
-  var die3 = $("#die3").removeClass("oneface twoface threeface fourface fiveface sixface");
-  var die4 = $("#die4").removeClass("oneface twoface threeface fourface fiveface sixface");
-  var die5 = $("#die5").removeClass("oneface twoface threeface fourface fiveface sixface");
+$("#roll").click(rollEachDie);
 
-//animate rolling
-function diceAction(){
- die1.animate({marginLeft: (Math.random()*2)* 100}, 150).addClass("rotate");
- die2.animate({marginLeft: (Math.random()*2.3)* 100}, 150);
- die3.animate({marginLeft: (Math.random()*2)* 100}, 150).addClass("rotate2");
- die4.animate({marginLeft: (Math.random()*2.3)* 100}, 150);
- die5.animate({marginLeft: (Math.random()*2)* 100}, 150).addClass("rotate3");
+function rollEachDie() {
+  var rolls = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0};
+  for (var i = 0; i < 6; i++) {
+    var roll = rollDie("#die" + i);
+    rolls[roll]++;
+  }
+
+  displayScores(rolls);
+
 }
 
-diceAction();
-
-
-  var dice = {
-    die1: random(),
-    die2: random(),
-    die3: random(),
-    die4: random(),
-    die5: random()
-  };
 
 //check if yacht
 if (die1 == die2 == die3 == die4 == die5){
@@ -63,82 +42,55 @@ if (die1 == die2 == die3 == die4 == die5){
 
 
 //count up number of numbers and add dice faces
-  var ones = 0;
-  var twos = 0;
-  var threes = 0;
-  var fours = 0;
-  var fives = 0;
-  var sixes = 0;
-
-  for (i=1; i<6; i++){
-    var die = $("#die" + i);
-    var roll = dice["die" + i];
-
-    if (roll == 1) {
-      die.addClass("oneface");
-      ones++;
-    } else if (roll == 2) {
-      die.addClass("twoface");
-      twos++;
-    } else if (roll == 3) {
-      die.addClass("threeface");
-      threes++;
-    } else if (roll == 4) {
-      die.addClass("fourface");
-      fours++;
-    } else if (roll == 5) {
-      die.addClass("fiveface");
-      fives++;
-    } else if (roll == 6) {
-      die.addClass("sixface");
-      sixes++;
-    }
-  }
-  twos = (twos *2);
-  threes = (threes *3);
-  fours = (fours * 4);
-  fives = (fives * 5);
-  sixes = (sixes * 6);
+function displayScores(rolls) {
+  var ones = rolls[1];
+  var twos = (rolls[2] *2);
+  var threes = (rolls[3] *3);
+  var fours = (rolls[4] * 4);
+  var fives = (rolls[5] * 5);
+  var sixes = (rolls[6] * 6);
   console.log(ones, twos, threes, fours, fives, sixes);
 
 //display on scorecard
-  if(ones !== 0 ){
-  $("#ones").text(ones).addClass("redtext");
-} else {
-  $("#ones").text("");
-}
+  if(ones !== 0){
+    $("#ones").text(ones).addClass("redtext");
+  } else {
+    $("#ones").text("");
+  }
 
   if(twos !== 0){
-  $("#twos").text(twos).addClass("redtext");
-} else {
-  $("#twos").text("");
-}
+    $("#twos").text(twos).addClass("redtext");
+  } else {
+    $("#twos").text("");
+  }
 
   if (threes !== 0){
-  $("#threes").text(threes).addClass("redtext");
-} else {
-  $("#threes").text("");
-}
+    $("#threes").text(threes).addClass("redtext");
+  } else {
+    $("#threes").text("");
+  }
 
   if (fours !== 0){
-  $("#fours").text(fours).addClass("redtext");
-} else {
-  $("#fours").text("");
-}
+    $("#fours").text(fours).addClass("redtext");
+  } else {
+    $("#fours").text("");
+  }
 
   if (fives !== 0){
-  $("#fives").text(fives).addClass("redtext");
-} else {
-  $("#fives").text("");
-}
+    $("#fives").text(fives).addClass("redtext");
+  } else {
+    $("#fives").text("");
+  }
 
   if (sixes !== 0){
-  $("#sixes").text(sixes).addClass("redtext");
-} else {
-  $("#sixes").text("");
+    $("#sixes").text(sixes).addClass("redtext");
+  } else {
+    $("#sixes").text("");
+  }
 }
 
-}
+
+
 
 //lock in score
 function lockin(){
@@ -147,7 +99,7 @@ function lockin(){
 
 function resetDie(id, dieFace) {
   var die = $(id);
-  die.removeClass("oneface twoface threeface fourface fiveface sixface rotate rotate2 rotate3");
+  die.removeClass("face1 face2 face3 face4 face5 face6 rotate rotate2 rotate3");
   die.addClass(dieFace);
   die.animate({marginLeft: 5});
 }
@@ -158,11 +110,11 @@ function reset(){
   console.log("reset");
 
   //reset dice
-  resetDie("#die1", "oneface");
-  resetDie("#die2", "twoface");
-  resetDie("#die3", "threeface");
-  resetDie("#die4", "fourface");
-  resetDie("#die5", "fiveface");
+  resetDie("#die1", "face1", "active");
+  resetDie("#die2", "face2", "active");
+  resetDie("#die3", "face3", "active");
+  resetDie("#die4", "face4", "active");
+  resetDie("#die5", "face5", "active");
 
   //reset board
   $("#ones").text("").removeClass("redtext");
@@ -194,5 +146,3 @@ function diceAction(){
 /// add held function that will disable dice from being rolled
 
 // able to click score to confirm, and will not be over written on  next roll
-
-//animate dice spinning
